cd349f1d5d3095b68432c12922a363e73cf32598
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 19:24:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,26 +1,4 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status
-  attr_reader :amount
-
-  def initialize(name)
-    @balance = 1000
-    @status = "open"
-    @name = name
-  end
-
-  def deposit(amount)
-    @balance += amount
-  end
-
-  def display_balance
-    "Your balance is $#{@balance}."
-  end
-
-  def valid?
-    @status == "open" && @balance > 0
-  end
-
-  def close_account
-    @status = "closed"
-  end
-end 
+  attr_accessor :sender, :receiver, :status, :amount
+  
+ 
