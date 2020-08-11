3413615c80e52691e68720a49b2021acfa455f7f
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 19:15:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,15 +7,20 @@ class BankAccount
     @status = open 
   end 
   
-  def deposit
+  def deposit(amount)
     @balance += amount
   end 
   
   def display_balance
-    "Your balance is #{balance}."
+    "Your balance is $#{self.balance}."
   end 
   
   def valid?
+    if self.balance > 0 && self.status == "open"
+      true 
+    else 
+      false 
+    end 
   end 
   
   def close_account 
