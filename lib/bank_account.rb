33f1360372c598456412d78aed872ab490b7f672
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 19:29:07 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,25 +3,27 @@ class BankAccount
   attr_accessor :balance, :status
   
   def initialize(name)
-    self.name = name 
-    self.balance = 1000
-    self.status = open 
-  end 
-  
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
   def deposit(amount)
-    self.balance += amount
-  end 
-  
+    @balance = @balance + amount 
+  end
+
   def display_balance
     "Your balance is $#{self.balance}."
-  end 
-  
-  def valid?
-    self.balance > 0 && self.status == "open"
-  end 
-  
-  def close_account 
+  end
+
+  def valid? 
+    if self.balance > 0 && self.status == "open"
+      true 
+    else 
+      false 
+    end
+  end
+
+  def close_account
     self.status = "closed"
-  end 
-    
-end
+  end
