32250206a48bfc01db531aba94ea3d5be8832eb8
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 19:12:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
-
+  attr_accessor :name, :balance, :status
+  
+  def initialize(account_name)
+  end 
+  
+  def deposit
+  end 
+  
+  def display_balance
+  end 
+  
+  def valid?
+  end 
+  
+  def close_account 
+  end 
+    
 end
