f84841c23f80f39bd1234609c18c2937a97820d9
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 19:21:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,33 +2,25 @@ class Transfer
   attr_accessor :sender, :receiver, :status
   attr_reader :amount
 
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @status = "pending"
-    @amount = amount
+  def initialize(name)
+    @balance = 1000
+    @status = "open"
+    @name = name
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
   end
 
   def valid?
-    @sender.valid? && @receiver.valid?
+    @status == "open" && @balance > 0
   end
 
-  def execute_transaction
-    if @amount > @sender.balance || @sender.status == "closed" || @receiver.status == "closed"
-     @status = "rejected"
-     "Transaction rejected. Please check your account balance."
-    elsif @status != "complete"
-    @sender.balance -= @amount
-    @receiver.balance += @amount
-    @status = "complete"
-    end
+  def close_account
+    @status = "closed"
   end
-
-  def reverse_transfer
-    if @status == "complete"
-    @receiver.balance -= @amount
-    @sender.balance += @amount
-    end
-    @status = "reversed"
-  end
-end
+end 
